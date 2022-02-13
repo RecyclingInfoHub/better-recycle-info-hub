17a3634f09da51f2cdf6e431089e53a922151318
Kelok Chan <kelokchan@gmail.com>
2022-02-13 15:09:52 +0800

    fix converter

--- src/firebase/locations.js
+++ src/firebase/locations.js
@@ -2,13 +2,17 @@ import { collection, getFirestore } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import app from "gatsby-plugin-firebase-v9.0";
 
+const converter = {
+  toFirestore: data => data,
+  fromFirestore: snapshot => ({
+    ...snapshot.data(),
+    id: snapshot.id,
+  }),
+};
+
 export const useLocations = () => {
-  const locationRef = collection(getFirestore(app), "locations").withConverter({
-    toFirestore: data => data,
-    fromFirestore: snapshot => ({
-      ...snapshot.data(),
-      id: snapshot.id,
-    }),
-  });
+  const locationRef = collection(getFirestore(app), "locations").withConverter(
+    converter
+  );
   return useCollectionData(locationRef);
 };
