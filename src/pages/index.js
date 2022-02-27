018fea68f2a32226e41572705c462f1326c45150
Darky <darkychin95@gmail.com>
2022-02-27 20:57:06 +0800

    Update index.js
    + Add ButtonRunBatch

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { graphql } from "gatsby";
 import Layout from "../components/layouts/AppLayout";
 import Seo from "../components/Seo";
 import LinkImage from "../components/LinkImage";
+// eslint-disable-next-line no-unused-vars
+import ButtonRunBatch from "../components/ButtonRunBatch";
 
 import paperImage from "../images/paper.png";
 import plasticImage from "../images/plastics.png";
@@ -15,29 +17,21 @@ import batteryImage from "../images/car-battery-icon.jpg";
 const Index = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`;
 
-  const videoList1 = [];
-  const videoList2 = [];
-
-  for (let i = 0; i < 4; i++) {
-    videoList1.push({
-      cardTitle: "Get better to recycle",
-      embedUrl: "https://www.youtube.com/embed/lZCXfWPtY78",
-    });
-    videoList2.push({
-      cardTitle: "Understand Recycle",
-      embedUrl: "https://www.youtube.com/embed/Umu-Hhv6qPA",
-    });
-  }
-
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="Better Recycle Info Hub" />
+      {/* 
+      Uncomment to use - 27/02/2022 Darky
+      <div className="w-1/2 mx-auto">
+        <ButtonRunBatch />
+      </div> 
+      */}
       <div className="grid grid-cols-3 gap-4 mx-auto justify-center w-1/2">
         <LinkImage image={paperImage} title="Paper" to={"/"} />
         <LinkImage image={plasticImage} title="Plastics" to={"/"} />
         <LinkImage image={glassImage} title="Glass" to={"/"} />
         <LinkImage image={metalImage} title="Tin &amp; Metal" to={"/"} />
-        <LinkImage image={electronicImage} title="Electornics" to={"/"} />
+        <LinkImage image={electronicImage} title="Electronics" to={"/"} />
         <LinkImage image={batteryImage} title="Battery" to={"/"} />
       </div>
     </Layout>
