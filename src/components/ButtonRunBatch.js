3c30bbde6b3745d726cf862cd0bfd69f549bf9e3
Darky <darkychin95@gmail.com>
2022-03-07 10:04:18 +0800

    Update ButtonRunBatch.js
    + Add loading spinner

--- src/components/ButtonRunBatch.js
+++ src/components/ButtonRunBatch.js
@@ -1,17 +1,41 @@
-import * as React from "react";
+import React, { useState } from "react";
 
 import { writeBatchFromJSON as writeBatchMaterial } from "../firebase/batch/material";
 import { writeBatchFromJSON as writeBatchLocation } from "../firebase/batch/location";
 
-// Execute batch
-const runBatch = async () => {
-  await writeBatchMaterial();
-  await writeBatchLocation();
-};
-
 const ButtonRunBatch = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Execute batch
+  const runBatch = async () => {
+    try {
+      setIsLoading(true);
+
+      await Promise.all([writeBatchMaterial, writeBatchLocation]);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const Spinner = () => {
+    if (isLoading) {
+      return (
+        <div className="animate-spin inline-block ml-1 w-4 h-4 border-1 rounded-full border-base-200">
+          {/* <span className="visually-hidden">Loading...</span> */}
+        </div>
+      )
+    }
+  };
+
   return (
-    <button className="btn-md btn-primary" onClick={runBatch}>Run Batch</button>
+    <button className="btn-md btn-primary" onClick={runBatch}>
+      Run Batch
+      {/* Bugged, spinner not showing */}
+      <Spinner />
+    </button>
   )
 };
 
